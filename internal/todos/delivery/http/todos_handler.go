b167ef9e6952f5d4f3adf1d6eb7e3b0d83457b16
Ori Avraham <ori.avraham@samsungnext.com>
2020-12-17 09:35:42 +0200

    better error response

--- internal/todos/delivery/http/todos_handler.go
+++ internal/todos/delivery/http/todos_handler.go
@@ -13,7 +13,22 @@ import (
 
 // ResponseError represent the reseponse error struct
 type ResponseError struct {
-	Message string `json:"message"`
+	HTTPStatus int    `json:"-"`
+	Code       int    `json:"code"`
+	Message    string `json:"message"`
+}
+
+func (e *ResponseError) WriteToResponse(w http.ResponseWriter) {
+	w.WriteHeader(e.HTTPStatus)
+	fmt.Fprint(w, e.ToJSON())
+}
+
+func (e *ResponseError) ToJSON() string {
+	j, err := json.Marshal(e)
+	if err != nil {
+		return `{"code":50001,"message":"unable to marshal error"}`
+	}
+	return string(j)
 }
 
 type ResponseMessage struct {
@@ -44,8 +59,8 @@ func (p *TodosHandler) searchTodos(w http.ResponseWriter, r *http.Request, ps ht
 
 	rec, err := p.TService.SearchTodos()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		log.Errorf("unable to read todo %v", err)
+		herr := &ResponseError{HTTPStatus: http.StatusBadRequest, Code: 40001, Message: "unable to search todos"}
+		herr.WriteToResponse(w)
 		return
 	}
 
@@ -64,8 +79,9 @@ func (p *TodosHandler) readTodo(w http.ResponseWriter, r *http.Request, ps httpr
 
 	rec, err := p.TService.ReadTodo(id)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
 		log.Errorf("unable to read todo %v", err)
+		herr := &ResponseError{HTTPStatus: http.StatusBadRequest, Code: 40001, Message: "Unable to read todo"}
+		herr.WriteToResponse(w)
 		return
 	}
 
@@ -85,8 +101,9 @@ func (p *TodosHandler) createTodo(w http.ResponseWriter, r *http.Request, ps htt
 
 	id, err := p.TService.CreateTodo(todo)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
 		log.Errorf("unable to read todo %v", err)
+		herr := &ResponseError{HTTPStatus: http.StatusBadRequest, Code: 40001, Message: "Unable to update todo"}
+		herr.WriteToResponse(w)
 		return
 	}
 
@@ -116,8 +133,9 @@ func (p *TodosHandler) updateTodo(w http.ResponseWriter, r *http.Request, ps htt
 
 	affected, err := p.TService.UpdateTodo(id, todo)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
 		log.Errorf("unable to update todo %v", err)
+		herr := &ResponseError{HTTPStatus: http.StatusBadRequest, Code: 40001, Message: "Unable to update todo"}
+		herr.WriteToResponse(w)
 		return
 	}
 
@@ -143,8 +161,9 @@ func (p *TodosHandler) deleteTodo(w http.ResponseWriter, r *http.Request, ps htt
 
 	affected, err := p.TService.DeleteTodo(id)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
 		log.Errorf("unable to delete todo %v", err)
+		herr := &ResponseError{HTTPStatus: http.StatusBadRequest, Code: 40001, Message: "Unable to delete todo"}
+		herr.WriteToResponse(w)
 		return
 	}
 
